a89a58ae9ff3b3358b5cf22b991ad5118efc0f60
Peter Banda <tremor11@gmail.com>
2019-07-18 20:59:18 +0200

    Version bump

--- app/org/ada/web/controllers/pdchallenge/QueryStringBinders.scala
+++ app/org/ada/web/controllers/pdchallenge/QueryStringBinders.scala
@@ -1,7 +1,7 @@
 package org.ada.web.controllers.pdchallenge
 
-import org.ada.web.controllers.EnumStringBindable
 import org.ada.web.models.pdchallenge.AggFunction
+import org.incal.play.formatters.EnumStringBindable
 
 object QueryStringBinders {
   implicit val aggFunctionQueryStringBinder = new EnumStringBindable(AggFunction)
